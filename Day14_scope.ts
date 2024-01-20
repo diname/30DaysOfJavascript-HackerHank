93e4d3a74abeabcba9fe344bc943d1eca4ed6a9e
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-20 12:54:54 -0300

    feat: Create Day 14: Scope with TypeScript

---
+++ Day14_scope.ts
@@ -0,0 +1,46 @@
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+let inputString: string = ''
+let inputLines: string[] = []
+let currentLine: number = 0
+process.stdin.on('data', function (inputStdin: string): void {
+  inputString += inputStdin
+})
+
+process.stdin.on('end', function (): void {
+  inputLines = inputString.split('\n')
+  inputString = ''
+  main()
+})
+
+function readLine(): string {
+  return inputLines[currentLine++]
+}
+
+class Difference {
+  private elements: number[]
+
+  constructor(elements: number[]) {
+    this.elements = elements
+  }
+
+  maximumDifference() {
+    const size = this.elements.length - 1
+    const difference: number = this.elements[size] - this.elements[0]
+    console.log(difference)
+  }
+}
+
+function main() {
+  // Enter your code here
+  const numUser: number[] = inputLines[1]
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b)
+
+  const difference: Difference = new Difference(numUser)
+
+  difference.maximumDifference()
+}
