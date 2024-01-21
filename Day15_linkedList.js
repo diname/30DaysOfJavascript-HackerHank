86506eaca0648c70798f6cc6f88583165b124d80
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-21 20:10:58 -0300

    Update file with the answer

--- Day15_linkedList.js
+++ Day15_linkedList.js
@@ -23,6 +23,22 @@ function Node(data) {
 function Solution() {
   this.insert = function (head, data) {
     //complete this method
+    const newNode = new Node(data)
+
+    if (head == null) {
+      head = newNode
+    } else if (head.next == null) {
+      head.next = newNode
+    } else {
+      let next = head.next
+
+      while (next.next) {
+        next = next.next
+      }
+
+      next.next = newNode
+    }
+    return head
   }
 
   this.display = function (head) {
