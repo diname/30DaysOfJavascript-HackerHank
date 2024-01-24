8b1a912247701eaf873865f923eca3ae8ed5cbd0
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-24 16:18:19 -0300

    feat: Update with the content

--- Day18_QueuesAndStacks.js
+++ Day18_QueuesAndStacks.js
@@ -19,6 +19,20 @@ function readLine() {
 
 function Solution() {
   //Write your code here
+  let stack = [],
+    queue = []
+  this.pushCharacter = (a) => {
+    return stack.push(a)
+  }
+  this.enqueueCharacter = (b) => {
+    return queue.unshift(b)
+  }
+  this.popCharacter = () => {
+    return stack.pop()
+  }
+  this.dequeueCharacter = () => {
+    return queue.pop()
+  }
 }
 
 function main() {
