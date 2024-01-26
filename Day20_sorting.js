44472a6b9892e16ad7aae34583efa8f88520ebd6
Lucas Medina <lucasmedinarmc@gmail.com>
2024-01-26 13:49:49 -0300

    feat: Adding the result of the challenge

--- Day20_sorting.js
+++ Day20_sorting.js
@@ -29,4 +29,28 @@ function main() {
     .map((aTemp) => parseInt(aTemp, 10))
 
   // Write your code here
+  var bubbleSort = function () {
+    let numberOfSwaps = 0
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        if (a[j] > a[j + 1]) {
+          var t1 = a[j]
+          var t2 = a[j + 1]
+          a[j] = t2
+          a[j + 1] = t1
+
+          numberOfSwaps++
+        }
+      }
+
+      if (numberOfSwaps == 0) {
+        break
+      }
+    }
+    return numberOfSwaps
+  }
+  var swapNum = bubbleSort()
+  console.log('Array is sorted in ' + swapNum + ' swaps.')
+  console.log('First Element: ' + a[0])
+  console.log('Last Element: ' + a[a.length - 1])
 }
